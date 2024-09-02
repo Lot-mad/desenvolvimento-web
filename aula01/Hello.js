f3af04772277d9fba2f9f5ffd4f62f66353ea2bb
Lot-mad <maria.ferreira4@estudante.ifgoiano.edu.br>
2024-09-02 10:21:37 -0300

    Introdução ao node

---
+++ aula01/Hello.js
@@ -0,0 +1,8 @@
+// node hello.js
+console.log('Olá do Servidor');
+
+/**
+ * node é a ferramente que vai nos entregar a capacidade de interpretar código js, de maneira bem similar ao navegador;
+ * node usa JS no servidor
+ * é um ambiente de servidor open source
+ */
